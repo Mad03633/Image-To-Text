e6d5a952352b750460381ad8382900796363ef8a
75776140 <madiyar03633@gmail.com>
2025-09-03 23:02:34 +0500

    update: ocr_service.py file

--- backend/ocr_service.py
+++ backend/ocr_service.py
@@ -1,19 +1,20 @@
 from PIL import Image
 import pytesseract
-from langdetect import detect
+import langid
 from config import LANG_MAP
 
+
 class OCRService:
-    def __init__(self, default_langs = "eng+kaz+rus"):
+    def __init__(self, default_langs="eng+kaz+rus"):
         self.default_langs = default_langs
 
     def extract_text(self, image: Image.Image):
         rough_text = pytesseract.image_to_string(image, lang=self.default_langs)
-        
+
         if not rough_text.strip():
             return {"text": "", "lang": "unknown", "language": "Unknown"}
 
-        detected_lang = detect(rough_text)
+        detected_lang, _ = langid.classify(rough_text)
 
         if detected_lang in LANG_MAP:
             tess_lang, human_lang = LANG_MAP[detected_lang]
@@ -21,9 +22,9 @@ class OCRService:
         else:
             final_text = rough_text
             human_lang = "Unknown"
-        
-        return{
-            "text": final_text,
+
+        return {
+            "text": final_text.strip(),
             "lang": detected_lang,
-            "language": human_lang
+            "language": human_lang,
         }
